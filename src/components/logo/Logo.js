f25dd38db2bd09b7220014c0be1d4c2d73abcf45
Jill Marcum <jill.marcum@gmail.com>
2020-03-09 19:07:17 +0000

    fixed folder names

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -6,8 +6,8 @@ import brain from './brain.png';
 const Logo = () => {
   return(
   <div className="ma4 mt0">
-    <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 250, width: 250 }} >
-      <div className="Tilt-inner pa3"><img src={brain} style={{paddingTop: '5px'}}alt='smartbrain logo'/></div>
+    <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 100, width: 100 }} >
+      <div className="Tilt-inner pa3"><img src={brain} style={{paddingTop: '0px'}}alt='smartbrain logo'/></div>
     </Tilt>
   </div>
   );
