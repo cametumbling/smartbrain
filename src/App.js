7abbb25b827840fdf4fd2c137be96382dd1efcbe
Jill Marcum <jill.marcum@gmail.com>
2020-03-12 13:48:53 +0000

    added frontend security measures (validation)

--- src/App.js
+++ src/App.js
@@ -38,10 +38,7 @@ const particlesOptions ={
       }
 }
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
+const initialState = {
       input: '',
       imageUrl: '',
       box: {},
@@ -54,8 +51,15 @@ class App extends React.Component {
         entries: 0,
         joined: ''
       }
+
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = initialState
     }
-  }
+
 
 loadUser = (data) => {
   this.setState({
@@ -115,6 +119,7 @@ loadUser = (data) => {
           .then(count => {
             this.setState(Object.assign(this.state.user, {entries: count}))
             })
+          .catch(console.log)
 
       }
       this.displayFaceBox(this.calculateFaceLocation(response))
@@ -124,7 +129,7 @@ loadUser = (data) => {
 
   onRouteChange = (route) => {
     if(route === 'signout'){
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home'){
       this.setState({isSignedIn: true})
     }
